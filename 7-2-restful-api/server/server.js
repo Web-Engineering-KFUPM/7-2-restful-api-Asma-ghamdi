b9c9aa14c7a4c31559adcd3fff724532e9658669
Asma <asmaalghamdi95@gmail.com>
2026-04-15 12:59:32 +0300

    todo 1&2

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import cors from "cors";
 
 // import dotenv and load environment variables from .env
-
+import dotenv from "dotenv";
+dotenv.config();    
 
 import { connectDB } from "./db.js";
 import { Song } from "./models/song.model.js";
@@ -26,3 +27,9 @@ await connectDB(process.env.MONGO_URL);
 // /api/songs/:id (Delete song)
 
 app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
+
+await mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
