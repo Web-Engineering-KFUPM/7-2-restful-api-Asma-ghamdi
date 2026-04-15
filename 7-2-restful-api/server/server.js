3b8ff9024ade06bbe35e3f46ca1e1c6083a46b38
Asma <asmaalghamdi95@gmail.com>
2026-04-15 13:21:26 +0300

    Todo 4

--- 7-2-restful-api/server/server.js
+++ 7-2-restful-api/server/server.js
@@ -16,15 +16,23 @@ app.use(cors());
 app.use(express.json());
 
 // api/songs (Read all songs)
+      app.get("/api/songs", async (req, res) => {
+        const rows = await Song.find().sort({ createdAt: -1 });
+        res.json(rows);
+      });
 
-
-// api/songs (Insert song)
+      app.get("/api/songs/:id", async (req, res) => {
+        const s = await Song.findById(req.params.id);
+        if (!s) return res.status(404).json({ message: "Song not found" });
+        res.json(s);
+      });
 
 // /api/songs/:id (Update song)
 
 
 // /api/songs/:id (Delete song)
 
+// api/songs (Insert song)
 app.post("/api/songs", async (req, res) => {
     try {
         const { title = "", artist = "", year } = req.body || {};
